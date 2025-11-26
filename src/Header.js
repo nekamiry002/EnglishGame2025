24ddbb24f44d8e3cd81f4ef2bf5541534de8f0cc
nekamiry002 <keryan.houdart@gmail.com>
2025-11-26 10:59:27 +0100

    try fix responsive

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,25 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import { useEffect, useRef } from "react";
 
 export default function Header() {
+    
+    const headerRef = useRef(null);
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (headerRef.current) {
+        const h = headerRef.current.offsetHeight;
+        document.documentElement.style.setProperty("--header-height", `${h}px`);
+      }
+    };
+
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
   return (
     <div className="main-header">
       <NavLink to="/rules" className="header-link">
